ca41818fc2f65e99566749e2edac544b96646ccd
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-09 14:08:40 -0500

    generating and validating passwords

---
+++ .idea/.idea.PasswordRetriever/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+L
+PasswordRetriever/Program.cs,0/5/05c6137fe7a161b8eff60d1ab7643581edace413
+Z
+*PasswordRetriever/PasswordRetriever.csproj,9/2/92d0a1db08557e2897b988d7d54378f0ee0126bb
+E
+PasswordRetriever.sln,d/d/dd7d83f15888a5dbf8bcc342dca7c600edf458a4
+F
+PasswordRetriever/.env,9/6/96f69e4e18e652808ad1ec34d5bab6a7501cc97a
--- PasswordRetriever/Program.cs
+++ PasswordRetriever/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+using OtpNet;
+
+var envPath = args[0] ?? ".env";
+
+FileStream fileStream = new FileStream(envPath, FileMode.Open, FileAccess.Read);
+
+using StreamReader reader = new StreamReader(fileStream);
+var key = Encoding.ASCII.GetBytes(reader.ReadLine()!.Split("=")[1]);
+reader.Close();
+
+var totp = new Totp(key);
+
+var uriString = new OtpUri(OtpType.Totp, key, "sambobbarnes", "Server Pass Retriever").ToString();
+Console.WriteLine("The URI is: {0}", uriString);
+
+Console.WriteLine("The current TOTP is: {0}", totp.ComputeTotp());
+Console.WriteLine("Enter the TOTP: ");
+var input = Console.ReadLine();
+
+var valid = totp.VerifyTotp(input, out _);
+
+Console.WriteLine("The input: {0} is {1}", input, valid ? "valid" : "invalid");
