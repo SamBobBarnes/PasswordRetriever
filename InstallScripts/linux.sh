b29f7a9d2b7d788ed971a269a54f857ec2ff95d7
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-09 20:39:19 -0500

    update mv to cp

--- InstallScripts/linux.sh
+++ InstallScripts/linux.sh
@@ -3,8 +3,8 @@
 echo "Enter machine name in caps: "
 read machineName
 mkdir /opt/password
-mv PasswordRetriever /opt/password/PasswordRetriever
-mv libinfisical_c.so /opt/password/libinfisical_c.so
+cp PasswordRetriever /opt/password/PasswordRetriever
+cp libinfisical_c.so /opt/password/libinfisical_c.so
 printf "TOTP_KEY=\nINFISICAL_CLIENT_ID=\nINFISICAL_CLIENT_SECRET=\nINFISICAL_URL=" > /opt/password/.env
 mkdir /opt/password/bin
 printf "#! /bin/bash \n\nPasswordRetriever ${machineName} ../.env" > /opt/password/bin/password
