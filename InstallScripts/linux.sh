2061ca8e842f0b008a8284a15c86c662687ed092
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-09 20:38:46 -0500

    update installer

--- InstallScripts/linux.sh
+++ InstallScripts/linux.sh
@@ -2,9 +2,11 @@
 
 echo "Enter machine name in caps: "
 read machineName
+mkdir /opt/password
 mv PasswordRetriever /opt/password/PasswordRetriever
 mv libinfisical_c.so /opt/password/libinfisical_c.so
 printf "TOTP_KEY=\nINFISICAL_CLIENT_ID=\nINFISICAL_CLIENT_SECRET=\nINFISICAL_URL=" > /opt/password/.env
 mkdir /opt/password/bin
-printf "#! /bin/bash \n\n./PasswordRetriever ${machineName} ../.env" > /opt/password/bin/password
+printf "#! /bin/bash \n\nPasswordRetriever ${machineName} ../.env" > /opt/password/bin/password
 chmod +x /opt/password/bin/password
+ln -sf /opt/password/bin/password /usr/bin/password
