46b59a198236d0080e77cf71a9a6f201e47ea9ae
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-09 20:42:02 -0500

    updated paths in installer

--- InstallScripts/linux.sh
+++ InstallScripts/linux.sh
@@ -7,6 +7,6 @@ cp PasswordRetriever /opt/password/PasswordRetriever
 cp libinfisical_c.so /opt/password/libinfisical_c.so
 printf "TOTP_KEY=\nINFISICAL_CLIENT_ID=\nINFISICAL_CLIENT_SECRET=\nINFISICAL_URL=" > /opt/password/.env
 mkdir /opt/password/bin
-printf "#! /bin/bash \n\nPasswordRetriever ${machineName} ../.env" > /opt/password/bin/password
+printf "#! /bin/bash \n\n/opt/password/PasswordRetriever ${machineName} /opt/password/.env" > /opt/password/bin/password
 chmod +x /opt/password/bin/password
 ln -sf /opt/password/bin/password /usr/bin/password
