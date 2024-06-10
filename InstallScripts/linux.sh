0a39700a429d2103244566423b10b8b07abbdadf
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-09 20:50:56 -0500

    added inputs for secrets

--- InstallScripts/linux.sh
+++ InstallScripts/linux.sh
@@ -2,11 +2,21 @@
 
 echo "Enter machine name in caps: "
 read machineName
+echo "Enter Client ID: "
+read client_id
+echo "Enter Client Secret: "
+read client_secret
+echo "Enter Secret Server Url: "
+read url
+echo "Enter TOTP Key: "
+read totp_key
 mkdir /opt/password
 cp PasswordRetriever /opt/password/PasswordRetriever
 cp libinfisical_c.so /opt/password/libinfisical_c.so
-printf "TOTP_KEY=\nINFISICAL_CLIENT_ID=\nINFISICAL_CLIENT_SECRET=\nINFISICAL_URL=" > /opt/password/.env
+printf "TOTP_KEY=${totp_key}\nINFISICAL_CLIENT_ID=${client_id}\nINFISICAL_CLIENT_SECRET=${client_secret}\nINFISICAL_URL=${url}" > /opt/password/.env
 mkdir /opt/password/bin
 printf "#! /bin/bash \n\n/opt/password/PasswordRetriever ${machineName} /opt/password/.env" > /opt/password/bin/password
 chmod +x /opt/password/bin/password
 ln -sf /opt/password/bin/password /usr/bin/password
+echo "Installation complete!"
+echo
